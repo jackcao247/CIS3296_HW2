7e855ffb6de89b8f0ae09a248630eb806f967ec8
Bao Cao <jackcao247@gmail.com>
2026-09-10 16:10:33 -0400

    Minimum balance requirement

--- BankAcc/BankAccount.java
+++ BankAcc/BankAccount.java
@@ -12,13 +12,14 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise. return false;
+        if (balance - amount >= 50) {
+            balance = balance - amount
+            System.out.println("Withdrawl success");
+            return true;
+        }
         return false;
     }
-    
+
     public String getAccountSummary() {
         return accountHolder + ": $" + balance;
     }
