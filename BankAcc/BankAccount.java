ea0935d69d684c7069650d30575c43e6628cbffc
Bao Cao <jackcao247@gmail.com>
2026-09-10 16:47:50 -0400

    2 dollars fee

--- BankAcc/BankAccount.java
+++ BankAcc/BankAccount.java
@@ -12,10 +12,10 @@ public class BankAccount {
     }
 
     public boolean withdraw(double amount) {
-        // TODO:
-        // Withdraw the requested amount if sufficient funds are available.
-        // Return true if the withdrawal succeeds.
-        // Return false otherwise. return false;
+        if (balance >= amount + 2) {
+            balance = balance - (amount + 2);
+            return true;
+        }
         return false;
     }
     
